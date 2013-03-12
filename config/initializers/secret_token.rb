37f659c88eef73d40a97db0673d60ef05a9c070f
Reema <reema@Presentsoft-Tech-3.(none)>
2013-03-12 14:31:15 +0530

    second app generated using scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRailApp2::Application.config.secret_token = '09922b25454b8bd34fcc75f12df3284eb2a72595b8ebbe5fbc5a34da60cba62d33fce5f0f3d1f2f19831f10bdc182935c6c28cc6785bb5628d15f4fa7d34a907'
